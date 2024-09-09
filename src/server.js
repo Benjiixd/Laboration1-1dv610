9bc6997387df474b1711c16f2c68b80e3936d4eb
Benjamin <bk222mb@student.lnu.se>
2024-09-09 07:14:56 +0200

    made a server that can start

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+//import { router } from './routes/router.js';
+import helmet from 'helmet';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(helmet());
+app.use(cors());
+
+//app.use('/', router);
+
+const server = app.listen(3020, async () => {
+  try {
+    
+  } catch (err) {
+    console.error(err);
+  }
+  console.log(`Server running on port ${server.address().port}`);
+});
